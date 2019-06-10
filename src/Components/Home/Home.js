41f7a3ffeb59bef900a4f5fc789cf66f82a7287c
StasBalan <43076181+StasBalan@users.noreply.github.com>
2019-06-10 15:39:08 +0300

    created Home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import './style.css'
 
 class Home extends Component{
-    render () {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: ``,
+            isFetching: true
+        };
+    }
+
+
+    componentDidMount() {
+        fetch(`https://api.adviceslip.com/advice`)
+            .then((er) => er.json())
+            .then((data) => this.setState({data: data.slip.advice, isFetching: false}));
+    }
+
+    render() {
         return (
-            <section>
+            <section className='section__home section-home'>
                 <div className='container'>
-                    <h1>Home</h1>
+                    <div className='section-home__inner'>
+                        { this.state.isFetching ? <div className='loader' /> : <h1 className='section__title'>{this.state.data}</h1> }
+                    </div>
                 </div>
             </section>
         );
