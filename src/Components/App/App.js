186bb9250ce8501fda03b15eb77291bd7c4b1649
StasBalan <43076181+StasBalan@users.noreply.github.com>
2019-06-10 12:14:59 +0300

    created routing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,12 +3,24 @@ import React from 'react';
 import './App.css';
 
 import Header from '../Header/Header';
+import Home from '../Home/Home';
+import Cards from '../Cards/Cards';
+import Settings from '../Settings/Settings';
+import Word from '../Word/Word';
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-    </div>
+        <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/card' component={Cards}/>
+            <Route path='/word' component={Word}/>
+            <Route path='/setting' component={Settings}/>
+        </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './style.css';
 
+import { Link } from 'react-router-dom';
+
 function Header(){
     return (
         <header className='header'>
@@ -17,10 +19,10 @@ function Header(){
                         <span className='header__title-red'>D</span>
                     </h1>
                     <ul className='header__menu'>
-                        <li className='header__item header-item'>Home</li>
-                        <li className='header__item header-item'>Card</li>
-                        <li className='header__item header-item'>Word</li>
-                        <li className='header__item header-item'>Setting</li>
+                        <li className='header__item header-item'><Link to='/' className='header-item__link'>Home</Link></li>
+                        <li className='header__item header-item'><Link to='/card' className='header-item__link'>Card</Link></li>
+                        <li className='header__item header-item'><Link to='/word' className='header-item__link'>Word</Link></li>
+                        <li className='header__item header-item'><Link to='/setting' className='header-item__link'>Setting</Link></li>
                     </ul>
                 </div>
             </div>
