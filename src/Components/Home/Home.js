a0dd9b29314a44097219d5dc41ad70fcbe1e72a9
StasBalan <43076181+StasBalan@users.noreply.github.com>
2019-06-24 10:29:56 +0300

    refactoring Home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,51 +1,52 @@
 import React, { Component } from 'react';
+
 import './style.css'
 
 import Header from "../Header/Header";
 import CardsItems from "../Cards-items/Cards-items";
+
 import { connect } from 'react-redux';
 import * as actions from "../../actions/Actions";
 
+
 class Home extends Component{
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: ``,
-            isFetching: true
-        };
-    }
-
+    state = {
+        motivationText: ``,
+        isFetching: true
+    };
 
     componentDidMount() {
         fetch(`https://api.adviceslip.com/advice`)
             .then((er) => er.json())
-            .then((data) => this.setState({data: data.slip.advice, isFetching: false}));
+            .then((data) => this.setState({motivationText: data.slip.advice, isFetching: false}));
     }
 
     render() {
+        let { isFetching, motivationText } = this.state;
+        let { isShowingCards } = this.props;
         return (
             <>
-            <section className='section__home section-home'>
-                <Header />
-                    <h1 className='section__title'>Motivation</h1>
-                    <div className='section-home__inner'>
-                        { this.state.isFetching ?
-                            <div id="cube-loader">
-                                <div className="caption">
-                                    <div className="cube-loader">
-                                        <div className="cube loader-1"/>
-                                        <div className="cube loader-2"/>
-                                        <div className="cube loader-4"/>
-                                        <div className="cube loader-3"/>
+                <section className='section__home section-home'>
+                    <Header />
+                        <h1 className='section__title'>Motivation</h1>
+                        <div className='section-home__inner'>
+                            { isFetching ?
+                                <div id="cube-loader">
+                                    <div className="caption">
+                                        <div className="cube-loader">
+                                            <div className="cube loader-1"/>
+                                            <div className="cube loader-2"/>
+                                            <div className="cube loader-4"/>
+                                            <div className="cube loader-3"/>
+                                        </div>
                                     </div>
-                                </div>
-                             </div>
-                            : <p className='section__text'>{this.state.data}</p> }
-                    </div>
-            </section>
-                {this.props.isShowingCards ? <CardsItems/> : null }
-                </>
+                                 </div>
+                                : <p className='section__text'>{motivationText}</p> }
+                        </div>
+                </section>
+                { isShowingCards ? <CardsItems/> : null }
+            </>
         );
     }
 }
