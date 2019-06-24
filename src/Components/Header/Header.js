a96434f3a01a7063134cc4d14555419b5a782d6e
StasBalan <43076181+StasBalan@users.noreply.github.com>
2019-06-24 10:25:52 +0300

    refactoring Header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './style.css';
 
 import Sidebar from '../Sidebar/Sidebar';
 
-import logo from '../../img/Goodword.png'
-
-class Header extends Component {
+import logo from '../../img/Goodword.png';
 
 
-
-    render() {
-        return (
-            <header className='header'>
-                    <div className='header__inner'>
-                        <img className='header__logo' src={logo} alt=""/>
-                        <Sidebar />
-                    </div>
-            </header>
-        );
-    }
+function Header() {
+    return (
+        <header className='header'>
+            <Sidebar />
+            <div className='header__inner'>
+                <img className='header__logo'
+                     src={logo}
+                     alt="logo"/>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
